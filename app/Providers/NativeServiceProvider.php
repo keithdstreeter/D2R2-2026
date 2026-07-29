daa2d4055cae517269f0c3d5870cef2e4006baaa
keithdstreeter <keith.d.streeter@gmail.com>
2026-07-29 14:57:39 -0400

    Registered Firebase

--- app/Providers/NativeServiceProvider.php
+++ app/Providers/NativeServiceProvider.php
@@ -36,6 +36,8 @@ class NativeServiceProvider extends ServiceProvider
         return [
             \SRWieZ\NativePHP\Mobile\Screen\ScreenServiceProvider::class,
             \Native\Mobile\Providers\BrowserServiceProvider::class,
+            \Native\Mobile\Providers\PushNotificationsServiceProvider::class,
+        
         ];
     }
 }
