fd29c7f0611a179a1b54e7a18033c858715ff961
keithdstreeter <keith.d.streeter@gmail.com>
2026-08-08 17:15:34 -0400

    Changes to solve net status issue

--- app/Providers/NativeServiceProvider.php
+++ app/Providers/NativeServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Native\Mobile\Providers\BrowserServiceProvider;
+use Native\Mobile\Providers\NetworkServiceProvider;
+use Native\Mobile\Providers\PushNotificationsServiceProvider;
+use SRWieZ\NativePHP\Mobile\Screen\ScreenServiceProvider;
 
 class NativeServiceProvider extends ServiceProvider
 {
@@ -29,15 +33,15 @@ class NativeServiceProvider extends ServiceProvider
      * This is a security measure to prevent transitive dependencies from
      * automatically registering plugins without your explicit consent.
      *
-     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
+     * @return array<int, class-string<ServiceProvider>>
      */
     public function plugins(): array
     {
         return [
-            \SRWieZ\NativePHP\Mobile\Screen\ScreenServiceProvider::class,
-            \Native\Mobile\Providers\BrowserServiceProvider::class,
-            \Native\Mobile\Providers\PushNotificationsServiceProvider::class,
-        
+            ScreenServiceProvider::class,
+            BrowserServiceProvider::class,
+            PushNotificationsServiceProvider::class,
+            NetworkServiceProvider::class,
         ];
     }
 }
