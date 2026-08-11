2baaa8750eb4f22d3c5276e92cc218f8d58771cb
keithdstreeter <keith.d.streeter@gmail.com>
2026-08-11 12:11:53 -0400

    Update Fairgrounds map

--- resources/views/components/fairgrounds.blade.php
+++ resources/views/components/fairgrounds.blade.php
@@ -18,11 +18,11 @@ new #[Title('Fairgrounds Info')] class extends Component {
         // Get the Ride (need better names across the board)
         //$this->ride_id = UserSetting::get('ride') ?: null;
         // Make the Map Name (currently using PNG files)
-        $this->map1_url = 'Fair Map D2R2 2026.png';
+        $this->map1_url = 'D2R2 Fairgrounds Map Update.png';
 
-        $this->map2_url = 'D2R2 Map.png';
+        // $this->map2_url = 'D2R2 Map.png';
 
-        $this->map3_url = 'Fairgrounds Map.png';
+        // $this->map3_url = 'Fairgrounds Map.png';
     }
 };
 ?>
@@ -32,7 +32,7 @@ new #[Title('Fairgrounds Info')] class extends Component {
     {{-- Title and Back Button --}}
     <div class="mx-auto max-w-lg mt- 20">
         <div class="mb-6 px-4 flex items-center justify-between">
-            <h1 class="text-2xl font-bold text-gray-800">Fairgrounds - 3 Maps</h1>
+            <h1 class="text-2xl font-bold text-gray-800">Fairgrounds</h1>
 
             <a href="{{ route('home') }}" wire:navigate
                 class="text-sm font-medium text-ocean-500 hover:text-ocean-600 transition-colors">&larr; Back</a>
@@ -40,16 +40,17 @@ new #[Title('Fairgrounds Info')] class extends Component {
     </div>
 
     {{-- Images are not showing in the simulator --}}
-    <p>Overhead Satellite View</p>
+    {{-- <p>Overhead Satellite View</p> --}}
     <img src="{{ asset('images/' . $this->map1_url) }}" alt="Photo"
         class="h-auto w-full max-w-none [touch-action:pinch-zoom]">
     <br>
-    <p>Angle Overhead View</p>
+    {{-- <p>Angle Overhead View</p>
     <img src="{{ asset('images/' . $this->map2_url) }}" alt="Photo"
         class="h-auto w-full max-w-none [touch-action:pinch-zoom]">
     <br>
     <p>Detailed Drawing</p>
     <img src="{{ asset('images/' . $this->map3_url) }}" alt="Photo"
         class="h-auto w-full max-w-none [touch-action:pinch-zoom]">
+--}}
 
 </div>
